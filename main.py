5b7c29539abfa933b1733cd19c11bb7d3b3980ae
ropz6x <127341230+Ropzx@users.noreply.github.com>
2025-05-15 00:28:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,5 +25,6 @@ def callback():
     user = sp.current_user()
     return f"Hello, {user['display_name']}!"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
